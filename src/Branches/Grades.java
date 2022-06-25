cad9d1aa1b538e354be8b546140b13e14ce70d9b
NewAccount <NewAccount@DESKTOP-JUFF927.Home>
2022-06-25 16:22:12 -0400

    test for jrnkins

--- src/Branches/Grades.java
+++ src/Branches/Grades.java
@@ -5,6 +5,7 @@ public class Grades {
 	public static void main(String[] args) {
 		System.out.println("welcome to Kumon");
 		System.out.println("levels of kumon");
+		System.out.println("test for jenkins");
 	}
 
 }
